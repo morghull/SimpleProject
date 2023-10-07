e462ddf3df6619144813ab10c5024190a432b497
morghull <morghull@gmail.com>
2023-10-07 19:14:56 +0300

    Create EntitySettings.cs

---
+++ SimpleProject/Helpers/EntitySettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProject.Helpers
+{
+    public class EntitySettings
+    {
+        public string EntityName { get; set; }
+        public string EntityPluralName { get; set; }
+        public string XmlFilePath { get; set; }
+        public Dictionary<string, Type> EntityPropreties { get; set; }
+        public EntitySettings(string entityName)
+        {
+            EntityName = entityName;
+            EntityPluralName = NounHelper.GetPluralForm(entityName);
+            EntityPropreties = new Dictionary<string, Type>();
+        }
+    }
+}

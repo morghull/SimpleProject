81aae1d7b0b2f122f304a189ec63f1a3a3c235b5
morghull <morghull@gmail.com>
2023-10-08 23:41:32 +0300

    Create TitleAttribute.cs

---
+++ SimpleProject/Helpers/TitleAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProject.Helpers
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    sealed class TitleAttribute : Attribute
+    {
+        public string Title { get; }
+
+        public TitleAttribute(string title)
+        {
+            Title = title;
+        }
+    }
+}

e6ebaf2b5eccf3ce016f2189aa633d7203efb668
morghull <morghull@gmail.com>
2023-10-08 23:44:46 +0300

    add titles to objects properties

--- SimpleProject/DataObjects/Movie.cs
+++ SimpleProject/DataObjects/Movie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleProject.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,13 @@ namespace SimpleProject.DataObjects
 {
     public class Movie
     {
+        [Title("Назва")]
         public string Name { get; set; }
+        [Title("Назва мовою оригіналу")]
         public string OriginalName { get; set; }
+        [Title("Дата релізу")]
         public DateTime ReleaseDate { get; set; }
+        [Title("Ім'я файлу постеру")]
         public string PosterImageName { get; set; }
         public Movie() { }
         public Movie(string name, string originalName, DateTime releaseDate, string posterImageName)
--- SimpleProject/DataObjects/Patient.cs
+++ SimpleProject/DataObjects/Patient.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using SimpleProject.Helpers;
+using System;
 
 namespace SimpleProject.DataObjects
 {
     public class Patient
     {
+        [Title("Ім'я")]
         public string FirstName { get; set; }
+        [Title("Прізвище")]
         public string LastName { get; set; }
+        [Title("Дата народження")]
         public DateTime Birthday { get; set; }
+        [Title("Номер палати")]
         public int RoomNo { get; set; }
+        [Title("Домашня адреса")]
         public string HomeAdress { get; set; }
         public Patient() { }
         public Patient(string firstName, string lastName, DateTime birthday, int roomNo, string homeAdress)

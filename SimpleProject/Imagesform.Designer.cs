cb206a3adf461b09cf8b0493a311ca7fc07d8269
morghull <morghull@gmail.com>
2023-10-08 18:35:34 +0300

    add Imagesform

---
+++ SimpleProject/Imagesform.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SimpleProject
+{
+    partial class Imagesform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this._flowLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // _flowLayoutPanel
+            // 
+            this._flowLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._flowLayoutPanel.Controls.Add(this.panel1);
+            this._flowLayoutPanel.Location = new System.Drawing.Point(19, 17);
+            this._flowLayoutPanel.Name = "_flowLayoutPanel";
+            this._flowLayoutPanel.Size = new System.Drawing.Size(1035, 524);
+            this._flowLayoutPanel.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 250);
+            this.panel1.TabIndex = 0;
+            // 
+            // Imagesform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this._flowLayoutPanel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Imagesform";
+            this.Text = "Imagesform";
+            this._flowLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel _flowLayoutPanel;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ SimpleProject/Imagesform.cs
@@ -0,0 +1,91 @@
+﻿using SimpleProject.DataObjects;
+using SimpleProject.Helpers;
+using SimpleProject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleProject
+{
+    public partial class Imagesform : Form
+    {
+        private EntitySettings<Movie> _entitySettings;
+        private EntityHelper<Movie> _entityHelper;
+        private string directoryPath = @"./Images/";
+
+        private Panel selectedPannel;
+        public Imagesform()
+        {
+            InitializeComponent();
+            _entitySettings = new EntitySettings<Movie>();
+            IDataInitializer<Movie> dataInitializer = new MoviesDataInitializer();
+            _entityHelper = new EntityHelper<Movie>(_entitySettings, dataInitializer);
+            _entityHelper.CreateInitialData();
+
+            List<Movie> movies = _entityHelper.GetDataAsList();
+
+            _flowLayoutPanel.Margin = new Padding(0);
+
+            MouseEventHandler doClick = (target, mouseEventArgs) =>
+            {
+                Panel panel;
+                if (target.GetType() == typeof(Panel))
+                {
+                    panel = (Panel)target;
+                }
+                else
+                {
+                    panel = (Panel)((Control)target).Parent;
+                }
+                if (selectedPannel != null)
+                {
+                    selectedPannel.BackColor = Color.Transparent;
+                }
+                panel.BackColor = Color.FromArgb(255, 0, 255, 0);
+                selectedPannel = panel;
+            };
+
+            foreach (Movie movie in movies)
+            {
+                Panel panel = new Panel();
+                PictureBox pictureBox = new PictureBox();
+                Label label = new Label();
+                panel.Controls.Add(pictureBox);
+                panel.Controls.Add(label);
+
+                pictureBox.ImageLocation = String.Format("{0}{1}", directoryPath, movie.PosterImageName);
+                pictureBox.Size = new Size(100, 150);
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox.BorderStyle = BorderStyle.FixedSingle;
+                pictureBox.Location = new Point(6, 6);
+
+                label.Text = movie.Name + Environment.NewLine + movie.ReleaseDate.ToShortDateString();
+                label.Location = new Point(
+                    pictureBox.Location.X,
+                    pictureBox.Location.Y + pictureBox.Height + panel.Padding.Top);
+                label.Size = new Size(pictureBox.Width, 40);
+                label.TextAlign = ContentAlignment.TopCenter;
+
+                pictureBox.MouseClick += doClick;
+                label.MouseClick += doClick;
+
+                panel.Padding = new Padding(0);
+                panel.Size = new Size(
+                    2 * pictureBox.Location.X + pictureBox.Width,
+                    2 * pictureBox.Location.Y + pictureBox.Height + label.Height);
+
+                panel.MouseClick += doClick;
+
+                _flowLayoutPanel.Controls.Add(panel);
+            }
+
+        }
+    }
+}

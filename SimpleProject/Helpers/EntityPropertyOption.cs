f5c84645f318bb8f2bca46805f84dd57db355f1a
morghull <morghull@gmail.com>
2023-10-15 15:56:49 +0300

    Update EntityPropertyOption.cs

--- SimpleProject/Helpers/EntityPropertyOption.cs
+++ SimpleProject/Helpers/EntityPropertyOption.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace SimpleProject.Helpers
 {
+    /// <summary>
+    /// для збереження Опцій властивостей класу сутності
+    /// </summary>
     public class EntityPropertyOption
     {
         public string Name { get; set; }
@@ -13,6 +16,14 @@ namespace SimpleProject.Helpers
         public Type Type { get; set; }
         public bool IsMultiline { get; set; }
         public int ColumnWidth { get; set; }
+        /// <summary>
+        /// конструктор з початковими значеннями
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="title"></param>
+        /// <param name="type"></param>
+        /// <param name="isMultiline"></param>
+        /// <param name="columnWidth"></param>
         public EntityPropertyOption(string name, string title, Type type, bool isMultiline, int columnWidth)
         {
             this.Name = name;

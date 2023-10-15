fa102a0fdaea41d21c5bedb63a80e894337a5908
morghull <morghull@gmail.com>
2023-10-15 16:44:57 +0300

    Update OptionsAttribute.cs

--- SimpleProject/Helpers/OptionsAttribute.cs
+++ SimpleProject/Helpers/OptionsAttribute.cs
@@ -6,12 +6,19 @@ using System.Threading.Tasks;
 
 namespace SimpleProject.Helpers
 {
+    /// <summary>
+    /// клас для кастомних аннотацій, що описують опції для властивостей класу сутності
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     sealed class OptionsAttribute : Attribute
     {
-        public readonly string Title;
-        public bool IsMultiline { get; set; }
-        public int ColumnWidth { get; set; }
+        public readonly string Title;//для заголовку
+        public bool IsMultiline { get; set; }//для визначення чи текст є довгим і йому потрібно розширяти текстове поле
+        public int ColumnWidth { get; set; }//ширина колонки у гріді для відображення
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        /// <param name="title"></param>
         public OptionsAttribute(string title)
         {
             Title = title;

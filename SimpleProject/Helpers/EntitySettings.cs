3f775dcb23685fffa412da9809f2c3373d987b41
morghull <morghull@gmail.com>
2023-10-15 16:44:46 +0300

    Update EntitySettings.cs

--- SimpleProject/Helpers/EntitySettings.cs
+++ SimpleProject/Helpers/EntitySettings.cs
@@ -6,26 +6,42 @@ using System.Runtime.InteropServices;
 
 namespace SimpleProject.Helpers
 {
+    /// <summary>
+    /// містить налаштування сутності.
+    /// при створені об'єкту екземпляру цього класу йде зчитування
+    /// опцій для кожної властивості класу сутності
+    /// також йде збереження базових налаштувань таких як шлях до xml-файлу,
+    /// назва сутності в однині та в множині
+    /// також реалізує інтерфейс IPropertiesOption, щоб була змога
+    /// отримати список опцій не прив'язуючись до реалізації під конкретну сутність
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class EntitySettings<T> : IPropertiesOption
     {
-        public string EntityName { get; set; }
-        public string EntityPluralName { get; set; }
-        public string XmlFilePath { get; set; }
-        public List<EntityPropertyOption> PropertiesOptions { get; set; }
+        public string EntityName { get; set; }//назва сутності у однині
+        public string EntityPluralName { get; set; }//назва сутності у множині
+        public string XmlFilePath { get; set; }//шлях до xml-файлу
+        public List<EntityPropertyOption> PropertiesOptions { get; set; }//список опцій для кожної властивості класу сутності
+        /// <summary>
+        /// конструктор
+        /// </summary>
         public EntitySettings()
         {
-            Type entityType = typeof(T);
+            Type entityType = typeof(T);//визначаємо тип сутності
 
-            EntityName = entityType.Name;
-            EntityPluralName = NounHelper.GetPluralForm(EntityName);
-            XmlFilePath = String.Format(@"./{0}.xml", EntityPluralName);
-            PropertiesOptions = new List<EntityPropertyOption>();
+            EntityName = entityType.Name;//визначаємо назва сутності
+            EntityPluralName = NounHelper.GetPluralForm(EntityName);//визначаємо назву у множині
+            XmlFilePath = String.Format(@"./{0}.xml", EntityPluralName);//визначаємо шлях до файлу
+            PropertiesOptions = new List<EntityPropertyOption>();//створюємо пустий список для опцій
 
-            PropertyInfo[] properties = entityType.GetProperties();
+            PropertyInfo[] properties = entityType.GetProperties();//отримуємо список дескрипторів властивостей сутності
 
+            //для кожної властивості створ.ємо опції зчитуючи анотації та дескриптор властивості
             foreach (PropertyInfo property in properties)
             {
+                //зчитаємо опції з аннотації
                 var optionsAttribute = (OptionsAttribute)Attribute.GetCustomAttribute(property, typeof(OptionsAttribute));
+                //наповнюємо список опцій
                 PropertiesOptions.Add(new EntityPropertyOption(
                     property.Name,
                     optionsAttribute.Title,
@@ -35,6 +51,10 @@ namespace SimpleProject.Helpers
                     ));
             }
         }
+        /// <summary>
+        /// отримати список опцій для властивостей сутності
+        /// </summary>
+        /// <returns></returns>
         public List<EntityPropertyOption> GetPropertiesOptions()
         {
             return PropertiesOptions;

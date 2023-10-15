fd811cc715cf817799ab6c6a7a23cd4773dda168
morghull <morghull@gmail.com>
2023-10-15 15:56:16 +0300

    Update Movie.cs

--- SimpleProject/DataObjects/Movie.cs
+++ SimpleProject/DataObjects/Movie.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 
 namespace SimpleProject.DataObjects
 {
+    //приклад створення класу для обєктів умовних "фільмів"
+    //концепція така: є сутність Фільм, він має певні властивості(property)
+    //кожній властивості сутності відповідає властивість класу
+    //що надалі буде зберігати значення цих властивостей первного типу
+    //за допомогою класу для кастомного анотацій для властивостей
+    //ми визначаємо додаткові так звані "опції" для властивостей
+    //це - заголовок, ширина стовпця для гріду відображення
+    //та булеве значення - чи властивість класу містить довгий текст (чи потрібно розширяти поле вводу цього тексту)
     public class Movie
     {
         [Options("Назва", ColumnWidth = 150)]
@@ -17,7 +25,17 @@ namespace SimpleProject.DataObjects
         public DateTime ReleaseDate { get; set; }
         [Options("Ім'я файлу постеру", ColumnWidth = 200)]
         public string PosterImageName { get; set; }
+        /// <summary>
+        /// метод конструктор без параметрів
+        /// </summary>
         public Movie() { }
+        /// <summary>
+        /// метод-конструктор з початковими значеннями властивостей
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="originalName"></param>
+        /// <param name="releaseDate"></param>
+        /// <param name="posterImageName"></param>
         public Movie(string name, string originalName, DateTime releaseDate, string posterImageName)
         {
             Name = name;

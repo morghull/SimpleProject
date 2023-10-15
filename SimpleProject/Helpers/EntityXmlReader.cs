08b532a095c7bb81859d92d03a17889fcead0dfd
morghull <morghull@gmail.com>
2023-10-15 16:44:49 +0300

    Update EntityXmlReader.cs

--- SimpleProject/Helpers/EntityXmlReader.cs
+++ SimpleProject/Helpers/EntityXmlReader.cs
@@ -4,50 +4,66 @@ using System.Xml;
 
 namespace SimpleProject.Helpers
 {
+    /// <summary>
+    /// дженерік клас. зчитає xml-файл де повинні зберігатись дані щодо колекції сутностей
+    /// не прив'язуючись до конкрутної реалізації сутності
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class EntityXmlReader<T> where T : new()
     {
+        /// <summary>
+        /// прочитати xml-файл
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
         public List<T> Read(string fileName)
         {
-            List<T> entities = new List<T>();
+            List<T> entities = new List<T>();//створити пустий список
             try
             {
-                using (XmlReader xmlReader = XmlReader.Create(fileName))
+                using (XmlReader xmlReader = XmlReader.Create(fileName))//створили так званий "читач"
                 {
-                    xmlReader.Read();
-                    XmlDocument xmlDocument = new XmlDocument();
-                    xmlDocument.Load(xmlReader);
+                    xmlReader.Read();//прочитати файл
+                    XmlDocument xmlDocument = new XmlDocument();//створити пустий документ
+                    xmlDocument.Load(xmlReader);//вивантажити прочитаний файлу у створений документ
 
-                    XmlNode entityNodes = xmlDocument.DocumentElement;
+                    XmlNode entityNodes = xmlDocument.DocumentElement;//рутовий елемент
 
-                    foreach (XmlNode entityNode in entityNodes)
+                    foreach (XmlNode entityNode in entityNodes)//дочірні елементи до рутового
                     {
-                        T entity = new T();
-                        foreach (XmlNode entityAttributeNode in entityNode.ChildNodes)
+                        T entity = new T();//створення екземпляру сутності
+                        foreach (XmlNode entityAttributeNode in entityNode.ChildNodes)//до кожного дочірнього елементу взяти його дочірні
                         {
-                            string propertyName = entityAttributeNode.Name;
-                            string propertyValue = entityAttributeNode.InnerText;
+                            string propertyName = entityAttributeNode.Name;//отримати ім'я властивості суності
+                            string propertyValue = entityAttributeNode.InnerText;//отримати її значення
 
-                            SetProperty(entity, propertyName, propertyValue);
+                            SetProperty(entity, propertyName, propertyValue);//наповнення властівості сутності значенням
                         }
-                        entities.Add(entity);
+                        entities.Add(entity);//додати сутність у колекцію
                     }
                 }
             }
-            catch (Exception ex)
+            catch (Exception ex)//обробка помилок
             {
                 Console.WriteLine(String.Format("An error occurred: {0}", ex.Message));
             }
             return entities;
         }
+        /// <summary>
+        /// виконує наповнення властівості сутності значенням
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="propertyValue"></param>
         private void SetProperty(T entity, string propertyName, string propertyValue)
         {
-            var propertyInfo = typeof(T).GetProperty(propertyName);
+            var propertyInfo = typeof(T).GetProperty(propertyName);//отримати дескриптор властивості
 
             if (propertyInfo != null)
             {
-                Type propertyType = propertyInfo.PropertyType;
-                object typedValue = Convert.ChangeType(propertyValue, propertyType);
-                propertyInfo.SetValue(entity, typedValue);
+                Type propertyType = propertyInfo.PropertyType;//отримати тип властивості
+                object typedValue = Convert.ChangeType(propertyValue, propertyType);//конвертація данних
+                propertyInfo.SetValue(entity, typedValue);//наповнення данними властивості об'єкту сутності
             }
         }
     }
